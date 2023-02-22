45cb44de222cc650e2248e85daca5398b1f58aa9
Vaserrrz <v.aserrrz@gmail.com>
2023-02-22 16:10:10 -0400

    3cer commit del dia

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->autoIncrement();
-            $table->string('name');
+            $table->id();
+            $table->string('name',100);
             $table->string('email')->unique();
             $table->text('description')->nullable();
+            $table->integer('Nivel')->nullable();
+            $table->integer('telefono')->nullable();
+            $table->integer('whatsapp')->nullable();
+            $table->integer('telegram')->nullable();
+            $table->integer('idempresa')->nullable();
+            $table->integer('status')->nullable();
+            $table->dateTime('ultima conexion')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
