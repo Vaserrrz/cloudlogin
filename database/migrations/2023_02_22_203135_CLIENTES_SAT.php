22b7707d17e01f2e7e44fd7e44ed206731b36ee3
Vaserrrz <v.aserrrz@gmail.com>
2023-02-22 16:42:32 -0400

    5to commit

---
+++ database/migrations/2023_02_22_203135_CLIENTES_SAT.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Nivel', function (Blueprint $table){
+            $table->id('CLI_ID');
+            $table->integer('CLI_CI_RIF');
+            $table->string('CLI_RAZON');
+            $table->string('CLI_DIRECCION');
+            $table->string('CLI_DETALLE');
+            $table->id('CLI_ENCARGADO_ID');
+            $table->integer('CLI_TELF');
+            $table->integer('CLI_TELF2');
+            $table->integer('TELF_WHATSAPP');
+            $table->integer('TELF_TELEGRAM');
+            $table->string('CORREO')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('CLIENTES_SAT');
+    }
+};
